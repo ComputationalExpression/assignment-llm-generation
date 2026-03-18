b3d41cce2a2439504519f17b6799707a6f208ff1
dluman <dluman@allegheny.edu>
2026-03-17 20:55:33 -0400

    Adding infrastructure files

--- src/main.py
+++ src/main.py
@@ -1,16 +1,1 @@
-"""Simple hello world program for learners."""
-
-
-def get_greeting() -> str:
-    """Return the greeting message."""
-    return "Hello, World!"
-
-
-def main() -> None:
-    """Entry point: print greeting to console."""
-    message = get_greeting()
-    print(message)
-
-
-if __name__ == "__main__":
-    main()
+# TODO: Write exercise to use with the chat
